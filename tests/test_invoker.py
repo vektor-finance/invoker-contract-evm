b5a72b69cab837ed9cc1e1065510edad413e769f
abdullathedruid <abdulla.alkamil@gmail.com>
2021-08-08 20:01:29 +0100

    Test if invoke unequal length

---
+++ tests/test_invoker.py
@@ -0,0 +1,23 @@
+import pytest
+from brownie import Invoker, accounts
+import brownie
+
+
+@pytest.fixture(scope="module")
+def invoker():
+    return accounts[0].deploy(Invoker)
+
+
+@pytest.fixture(autouse=True)
+def shared_setup(fn_isolation):
+    pass
+
+
+def test_owner(invoker):
+    assert invoker.owner() == accounts[0]
+
+def test_should_revert_if_unequal_length(invoker,dai):
+    calldata_one = dai.transferFrom.encode_input(accounts[0],accounts[1],1000)
+    calldata_two = dai.transferFrom.encode_input(accounts[0],accounts[1],1000)
+    with brownie.reverts('to+data length not equal'):
+        invoker.invoke([dai.address], [calldata_one, calldata_two], {'from': accounts[0]})
