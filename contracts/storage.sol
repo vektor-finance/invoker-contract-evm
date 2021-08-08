aee67e00bdd236885b0b88cb8b14ec9194370d11
Abdulla <abdulla.alkamil@gmail.com>
2021-08-08 14:12:58 +0100

    Storage can handle address,uint,bytes

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -11,11 +11,31 @@ pragma solidity ^0.8.6;
 contract Storage {
     mapping(bytes32 => bytes32) cache;
 
+    // READ OPERATIONS
+
     function read(bytes32 _key) public view returns (bytes32) {
         return cache[_key];
     }
 
+    function readAddress(bytes32 _key) public view returns (address) {
+        return address(uint160(uint256(cache[_key])));
+    }
+
+    function readUint256(bytes32 _key) public view returns (uint256) {
+        return uint256(cache[_key]);
+    }
+
+    // WRITE OPERATIONS
+
     function write(bytes32 _key, bytes32 _value) public {
         cache[_key] = _value;
+    }
+
+    function writeUint256(bytes32 _key, uint256 _value) public {
+        cache[_key] = bytes32(_value);
+    }
+
+    function writeAddress(bytes32 _key, address _value) public {
+        cache[_key] = bytes32(uint256(uint160(_value)));
     }
 }
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,17 +1,40 @@
 import pytest
 
 from brownie import Storage, accounts
+from brownie.test import given, strategy
+from brownie.convert import to_bytes
 
-#msg.sender = 0xb2f2618cecbbb6e7468cc0f2aa43858ad8d153e0280b22285e28e853bb9d453a
+KECCAK_MSGSENDER = '0xb2f2618cecbbb6e7468cc0f2aa43858ad8d153e0280b22285e28e853bb9d453a'
+KECCAK_BALANCE = '0xea06f38f7e4f15e87567361213c28f235cccdaa1d7fd34c9db1dfe9489c6a091'
+KECCAK_BYTES = '0xb963e9b45d014edd60cff22ec9ad383335bbc3f827be2aee8e291972b0fadcf2'
+KECCAK_BOOL = '0xc1053bdab4a5cf55238b667c39826bbb11a58be126010e7db397c1b67c24271b'
+NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 @pytest.fixture(scope="module")
 def storage():
     return accounts[0].deploy(Storage)
     
+@pytest.fixture(autouse=True)
+def shared_setup(fn_isolation):
+    pass
 
 def test_null(storage):
     assert storage.read('') == '0x'
 
-def test_set_sender(storage):
-    storage.write('0xb2f2618cecbbb6e7468cc0f2aa43858ad8d153e0280b22285e28e853bb9d453a',accounts[0].address)
-    assert storage.read('0xb2f2618cecbbb6e7468cc0f2aa43858ad8d153e0280b22285e28e853bb9d453a') == accounts[0].address
+@given(address=strategy('address'))
+def test_set_sender(storage, address):
+    storage.writeAddress(KECCAK_MSGSENDER,address)
+    assert storage.readAddress(KECCAK_MSGSENDER) == address
+
+def test_ensure_reset(storage):
+    assert storage.readAddress(KECCAK_MSGSENDER) == NULL_ADDRESS
+
+@given(value=strategy('uint256'))
+def test_set_value(storage, value):
+    storage.writeUint256(KECCAK_BALANCE, value)
+    assert storage.readUint256(KECCAK_BALANCE) == value
+
+@given(test_bytes=strategy('bytes32'))
+def test_set_bytes(storage, test_bytes):
+    storage.write(KECCAK_BYTES, test_bytes)
+    assert to_bytes(storage.read(KECCAK_BYTES)) == test_bytes
