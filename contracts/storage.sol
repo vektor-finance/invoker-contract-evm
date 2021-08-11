b627df122e35e683ae396c54bd5d408024ff7e17
abdullathedruid <abdulla.alkamil@gmail.com>
2021-08-11 19:46:11 +0100

    build: add interface for storage.sol

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -8,24 +8,26 @@
 
 pragma solidity ^0.8.6;
 
-contract Storage {
-    mapping(bytes32 => bytes32) cache;
+import "../interfaces/Istorage.sol";
+
+contract Storage is IStorage{
+    mapping(bytes32 => bytes32) private cache;
 
     // READ OPERATIONS
 
-    function read(bytes32 _key) public view returns (bytes32) {
+    function read(bytes32 _key) public view override returns (bytes32) {
         return cache[_key];
     }
 
-    function readAddress(bytes32 _key) public view returns (address) {
+    function readAddress(bytes32 _key) public view override  returns (address) {
         return address(uint160(uint256(cache[_key])));
     }
 
-    function readUint256(bytes32 _key) public view returns (uint256) {
+    function readUint256(bytes32 _key) public view override  returns (uint256) {
         return uint256(cache[_key]);
     }
 
-    function readBool(bytes32 _key) public view returns (bool) {
+    function readBool(bytes32 _key) public view override  returns (bool) {
         if (cache[_key] == bytes32(uint256(1))) {
             return true;
         } else {
@@ -35,19 +37,19 @@ contract Storage {
 
     // WRITE OPERATIONS
 
-    function write(bytes32 _key, bytes32 _value) public {
+    function write(bytes32 _key, bytes32 _value) public override  {
         cache[_key] = _value;
     }
 
-    function writeUint256(bytes32 _key, uint256 _value) public {
+    function writeUint256(bytes32 _key, uint256 _value) public override  {
         cache[_key] = bytes32(_value);
     }
 
-    function writeAddress(bytes32 _key, address _value) public {
+    function writeAddress(bytes32 _key, address _value) public override  {
         cache[_key] = bytes32(uint256(uint160(_value)));
     }
 
-    function writeBool(bytes32 _key, bool _value) public {
+    function writeBool(bytes32 _key, bool _value) public override  {
         if (_value) {
             cache[_key] = bytes32(uint256(1));
         } else {
