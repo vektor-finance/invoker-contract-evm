9ff65096fed3f2518d98c29a105e3ca16baf59da
abdullathedruid <abdulla.alkamil@gmail.com>
2021-08-13 22:19:03 +0100

    readme: Update info in deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,21 @@
+# Deployment script to deploy all contracts on hardhat fork
+# To run, please type `brownie run deploy` in console
+# The script will deploy the invoker contract, and all command contracts
+# Appropriate access control will also be initialised
+# The script will then continue to mine new blocks until closed
+# To connect with metamask (or alternate wallet), create a custom network
+# -- network name: Vektor test net
+# -- RPC url: http://127.0.0.1:8545
+# -- Chain ID: 1337
+# The other settings can be left blank
+# In the future, we could deploy/mint ether/erc20 tokens for users
+
+
 from brownie import CMove, CSwap, Invoker, accounts, chain, network
 from brownie.network.state import Chain
 
 commands = [CMove, CSwap]
-# Roles
 APPROVED_COMMAND = "410a6a8d01da3028e7c041b5925a6d26ed38599db21a26cf9a5e87c68941f98a"
-# keccak256("APPROVED_COMMAND_IMPLEMENTATION")
 
 
 def deploy_invoker(deployer):
