6629dec64e6eada32fe60d8fd3a8ef7f5d3180b3
abdullathedruid <abdulla.alkamil@gmail.com>
2021-08-17 10:16:04 +0100

    feat: add moveERC20In and Out

--- contracts/commands/CommandMove.sol
+++ contracts/commands/CommandMove.sol
@@ -11,6 +11,18 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract CMove {
     using SafeERC20 for IERC20;
 
+    function moveERC20In(IERC20 _token, uint256 amount) external {
+        _moveERC20(_token, msg.sender, address(this), amount);
+    }
+
+    function moveERC20Out(
+        IERC20 _token,
+        address _to,
+        uint256 _amount
+    ) external {
+        _moveERC20(_token, address(this), _to, _amount);
+    }
+
     /**
         @notice Allows a user to move their tokens to another address
         @dev Uses OpenZepellin SafeERC20, and validates balance before and after transfer
@@ -21,13 +33,13 @@ contract CMove {
         @param _to  The address you wish to send the tokens to
         @param _amount The amount of tokens to transfer
     **/
-    function moveERC20(
+    function _moveERC20(
         IERC20 _token,
+        address _from,
         address _to,
         uint256 _amount
-    ) external {
+    ) internal {
         uint256 balanceBefore = _token.balanceOf(_to);
-        address _from = msg.sender;
         _token.transferFrom(_from, _to, _amount);
         uint256 balanceAfter = _token.balanceOf(_to);
         require(balanceAfter == balanceBefore + _amount, "CMove: Deflationary token");
