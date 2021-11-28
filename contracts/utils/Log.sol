e7bb824595bc7529a8b0a7d0f2742cae0a436b39
abdullathedruid <abdulla.alkamil@gmail.com>
2021-11-28 01:52:19 +0000

    Rename LogVeks to LogVek (singular)

--- contracts/utils/Log.sol
+++ contracts/utils/Log.sol
@@ -10,19 +10,19 @@ contract Log {
     // An invocation will emit a log any time a user calls invoke()
     event LogInvocation(address indexed user, bytes4 indexed sigHash, bytes params, uint256 value);
     // For every command within the invocation, there will be a separate emitted event
-    event LogVeks(address indexed user, bytes4 indexed sigHash, bytes params, uint256 value);
+    event LogVek(address indexed user, bytes4 indexed sigHash, bytes params, uint256 value);
 
     modifier logInvocation() {
         emit LogInvocation(msg.sender, msg.sig, msg.data, msg.value);
         _;
     }
 
-    function logVeks(bytes calldata params) internal {
+    function logVek(bytes calldata params) internal {
         // Extract the function selector from calldata to allow for indexing
         bytes4 sigHash = (bytes4(params[0]) |
             (bytes4(params[1]) >> 8) |
             (bytes4(params[2]) >> 16) |
             (bytes4(params[3]) >> 24));
-        emit LogVeks(msg.sender, sigHash, params, msg.value);
+        emit LogVek(msg.sender, sigHash, params, msg.value);
     }
 }
