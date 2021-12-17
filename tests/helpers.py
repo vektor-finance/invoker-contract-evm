37bfd0ecf7e3d73862987706177f981d5f1f4fe1
Akram Hussein <akram@vektor.finance>
2021-12-17 01:20:56 -0300

    feat(cswap): add unwrapAllWeth to CSwap (#40)
    * feat(cswap): add unwrapAllWeth
    
    * feat(cswap): add test_unwrap_all_weth
    
    * fix: remove unnessary arg

--- tests/helpers.py
+++ tests/helpers.py
@@ -17,6 +17,13 @@ def get_world_token_for_user(user, weth, world, uni_router):
     assert world.balanceOf(user) > 0
 
 
+def get_weth(weth, invoker, value):
+    # maker contract has 1.8M WETH so we can "borrow" some for testing purposes
+    maker_contract = "0x2F0b23f53734252Bda2277357e97e1517d6B042A"
+    weth.transfer(invoker, value, {"from": maker_contract})
+    assert weth.balanceOf(invoker) == value
+
+
 # Roles
 APPROVED_COMMAND = "410a6a8d01da3028e7c041b5925a6d26ed38599db21a26cf9a5e87c68941f98a"
 # keccak256("APPROVED_COMMAND_IMPLEMENTATION")
