57955f4253379dd58c6ddb32551c521626ae75a5
abdullathedruid <abdulla@vektor.finance>
2022-02-21 15:54:46 +0000

    feat: support avalanche (#72)
    * feat: add avalanche assets
    
    * feat: add public avalanche RPC
    
    * feat: add avalanche networks
    
    * feat: hotfix for pangolin router
    
    * feat: add avalanche to CI/Makefile
    
    * feat: swap pangolin with trader joe
    
    * refactor: tests to use chain fixture
    
    * fix: dict for router abi
    
    * feat: remove explorer
    
    * fix: revert to using fn not dict

--- tests/integration/commands/test_swap.py
+++ tests/integration/commands/test_swap.py
@@ -4,13 +4,20 @@ import pytest
 from brownie.test import given, strategy
 from hypothesis import assume
 
-from data.chain import get_chain, get_chain_id
 
-chain_id = get_chain_id()
-chain = get_chain()
+def fn_native_for_token(uni_router, chain_id):
+    if chain_id == 43114:
+        return uni_router.swapExactAVAXForTokens
+    return uni_router.swapExactETHForTokens
 
 
-def test_buy_token(alice, wnative, uni_router, token):
+def fn_token_for_native(uni_router, chain_id):
+    if chain_id == 43114:
+        return uni_router.swapExactTokensForAVAX
+    return uni_router.swapExactTokensForETH
+
+
+def test_buy_token(alice, wnative, uni_router, token, connected_chain):
     if token == wnative:
         pytest.skip("Cannot buy WNATIVE")
     amount_in = 1e18
@@ -21,9 +28,9 @@ def test_buy_token(alice, wnative, uni_router, token):
     # This is a 'bad' example and should be ignored (not failed)
     assume(amount_out > 0)
 
-    uni_router.swapExactETHForTokens(
-        amount_out, path, alice, time.time() + 1, {"from": alice, "value": amount_in}
-    )
+    fn = fn_native_for_token(uni_router, connected_chain["chain_id"])
+
+    fn(amount_out, path, alice, time.time() + 1, {"from": alice, "value": amount_in})
     assert token.balanceOf(alice) >= amount_out
 
 
@@ -49,7 +56,7 @@ def test_buy_with_invoker(alice, wnative, uni_router, token, invoker, cswap, val
     assert token.balanceOf(invoker.address) >= amount_out
 
 
-def test_sell_token(alice, wnative, uni_router, tokens_for_alice):
+def test_sell_token(alice, wnative, uni_router, tokens_for_alice, connected_chain):
     if tokens_for_alice == wnative:
         pytest.skip("Cannot sell wnative")
     amount_in = 100 * (10 ** tokens_for_alice.decimals())
@@ -60,9 +67,9 @@ def test_sell_token(alice, wnative, uni_router, tokens_for_alice):
 
     assume(amount_out > 0)
 
-    uni_router.swapExactTokensForETH(
-        amount_in, amount_out, path, alice, time.time() + 1, {"from": alice}
-    )
+    fn = fn_token_for_native(uni_router, connected_chain["chain_id"])
+
+    fn(amount_in, amount_out, path, alice, time.time() + 1, {"from": alice})
     assert alice.balance() >= prev_balance + amount_out
 
 
