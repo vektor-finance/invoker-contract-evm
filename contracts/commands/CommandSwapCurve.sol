ed7cafb8faff5b6c959c0bf488452831ea42dc4d
abdullathedruid <abdulla@vektor.finance>
2022-02-24 10:40:23 +0000

    feat: swapCurve

--- contracts/commands/CommandSwapCurve.sol
+++ contracts/commands/CommandSwapCurve.sol
@@ -1,22 +1,27 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../../interfaces/Commands/Swap/ISwapTemplateCurve.sol";
 
 contract CSwapCurve {
     using SafeERC20 for IERC20;
 
     function swapCurve(
-        address _tokenIn,
-        address _tokenOut,
         uint256 _amountIn,
         uint256 _minAmountOut,
-        address _pool
+        address[2] calldata _tokens,
+        address _pool,
+        int128 _i,
+        int128 _j
     ) external payable {
-        (int128 i, int128 j, bool isUnderlying) = REGISTRY.get_coin_indices(_tokenIn, _tokenOut);
+        IERC20 tokenIn = IERC20(_tokens[0]);
+        IERC20 tokenOut = IERC20(_tokens[1]);
         _tokenIn.safeApprove(_pool, 0);
         _tokenIn.safeApprove(_pool, _amountIn);
-        POOL = POOL(_pool);
+        POOL = ISwapTemplateCurve(_pool);
         uint256 balanceBefore = _tokenOut.balanceOf(address(this));
-        if (isUnderlying) {}
+        POOL.exchange(_i, _j, _amountIn, _minAmountOut);
+        uint256 balanceAfter = _tokenOut.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore + _amountOutMin, "CSwap: Slippage in");
     }
 }
