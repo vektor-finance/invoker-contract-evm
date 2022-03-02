7018c175a0bc4697ca2ae9ccd808bdc6f7c77d17
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-03-02 12:01:49 +0000

    wip: prototype for buy

--- contracts/commands/CommandSwapCurve.sol
+++ contracts/commands/CommandSwapCurve.sol
@@ -7,8 +7,62 @@ import "../../interfaces/Commands/Swap/Curve/ICSwapCurve.sol";
 import "../../interfaces/Commands/Swap/Curve/ICurvePool.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+interface IRegistry {
+    function get_coin_indices(
+        address _pool,
+        address _from,
+        address _to
+    )
+        external
+        view
+        returns (
+            int128 i,
+            int128 j,
+            bool is_underlying
+        );
+
+    function get_A(address _pool) external view returns (uint256);
+
+    function get_fees(address _pool) external view returns (uint256[2] memory);
+
+    function get_n_coins(address _pool) external view returns (uint256[2] memory);
+
+    function get_underlying_balances(address _pool) external view returns (uint256[8] memory);
+
+    function get_underlying_decimals(address _pool) external view returns (uint256[8] memory);
+
+    function get_rates(address _pool) external view returns (uint256[8] memory);
+
+    function get_decimals(address _pool) external view returns (uint256[8] memory);
+
+    function get_balances(address _pool) external view returns (uint256[8] memory);
+}
+
+interface ICalculator {
+    function get_dx(
+        int128 n_coins,
+        uint256[8] calldata balances,
+        uint256 amp,
+        uint256 fee,
+        uint256[8] calldata rates,
+        uint256[8] calldata precisions,
+        bool is_underlying,
+        int128 i,
+        int128 j,
+        uint256 dy
+    ) external view returns (uint256);
+}
+
 contract CSwapCurve is ICSwapCurve {
     using SafeERC20 for IERC20;
+
+    IRegistry public immutable REGISTRY;
+    ICalculator public immutable CALCULATOR;
+
+    constructor(IRegistry registry, ICalculator calculator) {
+        REGISTRY = registry;
+        CALCULATOR = calculator;
+    }
 
     // https://github.com/curvefi/curve-pool-registry/blob/master/contracts/Swaps.vy#L446
     function sell(
@@ -57,6 +111,134 @@ contract CSwapCurve is ICSwapCurve {
 
         uint256 balanceAfter = tokenOut.balanceOf(address(this));
         require(balanceAfter >= balanceBefore + minAmountOut, "CSwap: Slippage in");
+    }
+
+    function buy(
+        uint256 amountOut,
+        uint256 maxAmountIn,
+        address[2] calldata tokens,
+        CurveSwapParams calldata params
+    ) external payable {
+        IERC20 tokenIn = IERC20(tokens[0]);
+        IERC20 tokenOut = IERC20(tokens[1]);
+        // Add 1 wei as rounding error may reduce amount of tokens received
+        uint256 inputAmount = 1 +
+            get_input_amount(
+                params.poolAddress,
+                amountOut,
+                GIAParams({
+                    i: int128(int256(params.tokenI)),
+                    j: int128(int256(params.tokenJ)),
+                    is_underlying: params.swapType % 2 == 0
+                })
+            );
+
+        require(inputAmount <= maxAmountIn, "CSwap: Slippage out");
+
+        tokenIn.safeApprove(params.poolAddress, 0);
+        tokenIn.safeApprove(params.poolAddress, inputAmount);
+
+        uint256 balanceBefore = tokenOut.balanceOf(address(this));
+
+        if (params.swapType == 1) {
+            // Stableswap `exchange`
+            ICurvePool(params.poolAddress).exchange(
+                int128(int256(params.tokenI)),
+                int128(int256(params.tokenJ)),
+                inputAmount,
+                0
+            );
+        } else if (params.swapType == 2) {
+            // Stableswap `exchange_underlying`
+            ICurvePool(params.poolAddress).exchange_underlying(
+                int128(int256(params.tokenI)),
+                int128(int256(params.tokenJ)),
+                inputAmount,
+                0
+            );
+        } else if (params.swapType == 3) {
+            // Cryptoswap `exchange`
+            ICryptoPool(params.poolAddress).exchange(params.tokenI, params.tokenJ, inputAmount, 0);
+        } else if (params.swapType == 4) {
+            // Cryptoswap `exchange_underlying`
+            ICryptoPool(params.poolAddress).exchange_underlying(
+                params.tokenI,
+                params.tokenJ,
+                inputAmount,
+                0
+            );
+        } else {
+            revert("CSwapCurve: Unknown swapType");
+        }
+
+        // TODO: potentially unnecessary check
+        uint256 balanceAfter = tokenOut.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore + amountOut, "CSwap: Slippage in");
+    }
+
+    struct CalculatorParams {
+        uint256 n_coins;
+        uint256[8] balances;
+        uint256 amp;
+        uint256 fee;
+        uint256[8] rates;
+        uint256[8] decimals;
+        int128 i;
+        int128 j;
+    }
+
+    struct GIAParams {
+        int128 i;
+        int128 j;
+        bool is_underlying;
+    }
+
+    function get_input_amount(
+        address pool,
+        uint256 amount,
+        GIAParams memory params
+    ) public view returns (uint256) {
+        CalculatorParams memory vars;
+        int128 nc;
+
+        vars.amp = REGISTRY.get_A(pool);
+        vars.fee = REGISTRY.get_fees(pool)[0];
+
+        vars.n_coins = REGISTRY.get_n_coins(pool)[params.is_underlying ? 1 : 0];
+        if (params.is_underlying) {
+            vars.balances = REGISTRY.get_underlying_balances(pool);
+            vars.decimals = REGISTRY.get_underlying_decimals(pool);
+            for (uint256 x = 0; x < 8; x++) {
+                if (x == vars.n_coins) {
+                    break;
+                }
+                vars.rates[x] = 10**18;
+            }
+        } else {
+            vars.balances = REGISTRY.get_balances(pool);
+            vars.decimals = REGISTRY.get_decimals(pool);
+            vars.rates = REGISTRY.get_rates(pool);
+        }
+        for (uint256 x = 0; x < 8; x++) {
+            if (x == vars.n_coins) {
+                break;
+            }
+            vars.decimals[x] = 10**(18 - vars.decimals[x]);
+        }
+
+        return
+            CALCULATOR.get_dx(
+                int128(int256(vars.n_coins)),
+                vars.balances,
+                vars.amp,
+                vars.fee,
+                vars.rates,
+                vars.decimals,
+                params.is_underlying,
+                params.i,
+                params.j,
+                amount
+            );
     }
 
     // need to consider how to handle native eth (alternatively, enforce wrapped eth)
--- tests/combination/mainnet-hardhat-fork/test_curve.py
+++ tests/combination/mainnet-hardhat-fork/test_curve.py
@@ -2,6 +2,7 @@
 
 import pytest
 from brownie import Contract
+from brownie.exceptions import VirtualMachineError
 
 from data.access_control import APPROVED_COMMAND
 from data.chain import get_chain
@@ -9,8 +10,12 @@ from data.curve import CURVE_ASSET_TYPE_CRYPTO, get_curve_pools
 
 
 @pytest.fixture(scope="module")
-def cswap_curve(invoker, deployer, CSwapCurve):
-    contract = deployer.deploy(CSwapCurve)
+def cswap_curve(invoker, deployer, CSwapCurve, provider):
+    # Exchanges contract
+    # CurveCalculator
+    contract = deployer.deploy(
+        CSwapCurve, provider.get_registry(), "0xc1DB00a8E5Ef7bfa476395cdbcc98235477cDE4E"
+    )
     invoker.grantRole(APPROVED_COMMAND, contract, {"from": deployer})  # approve command
     yield contract
 
@@ -18,7 +23,7 @@ def cswap_curve(invoker, deployer, CSwapCurve):
 @pytest.fixture(scope="module")
 def curve_dest(request, interface):
     dest_token = Contract.from_abi(
-        request.param["name"], request.param["address"], interface.IERC20.abi
+        request.param["name"], request.param["address"], interface.ERC20Detailed.abi
     )
     yield dest_token
 
@@ -54,7 +59,61 @@ def pytest_generate_tests(metafunc):
 
 
 @pytest.mark.only_curve_pool_tokens("tokens_for_alice", "curve_dest")
-def test_buysell_with_curve(
+def test_buy_with_curve(
+    curve_pool,
+    tokens_for_alice,
+    curve_dest,
+    invoker,
+    alice,
+    cswap_curve,
+    cmove,
+    interface,
+    registry,
+):
+    target_decimals = curve_dest.decimals()
+
+    value = 10 ** target_decimals
+    # underlying = False
+    is_crypto_pool = curve_pool.asset_type == CURVE_ASSET_TYPE_CRYPTO
+    pool = curve_pool.swap_address
+    (i, j, underlying) = registry.get_coin_indices(pool, tokens_for_alice, curve_dest)
+    params = [pool, i, j, None]
+
+    # if is_crypto_pool:
+    #     pool = Contract.from_abi("Curve Crypto Pool", pool, interface.CurveCryptoPool.abi)
+    # else:
+    #     pool = Contract.from_abi("Curve Pool", pool, interface.CurvePool.abi)
+
+    # if underlying:
+    #     amount_out = int(pool.get_dy_underlying(i, j, value) // 1.01)
+    # else:
+    #     amount_out = int(pool.get_dy(i, j, value) // 1.01)
+
+    try:
+        amount_in = cswap_curve.get_input_amount(pool, value, (i, j, underlying))
+    except VirtualMachineError:
+        pytest.skip("cant get amounts")
+    amount_in = int(amount_in // 0.99)
+
+    params[3] = (is_crypto_pool * 2) + underlying + 1
+
+    bal = tokens_for_alice.balanceOf(alice)
+    tokens_for_alice.approve(invoker, bal, {"from": alice})
+    calldata_move = cmove.moveERC20In.encode_input(tokens_for_alice, bal)
+    calldata_swap = cswap_curve.buy.encode_input(
+        value, amount_in, [tokens_for_alice, curve_dest], params
+    )
+
+    invoker.invoke([cmove, cswap_curve], [calldata_move, calldata_swap], {"from": alice})
+
+    print(f"Tried to buy {value / (10 ** target_decimals)} {curve_dest._name} tokens")
+    print(f"Received {curve_dest.balanceOf(invoker) / (10 ** target_decimals)}")
+    assert curve_dest.balanceOf(invoker) >= value
+
+
+"""
+@pytest.mark.only_curve_pool_tokens("tokens_for_alice", "curve_dest")
+def test_sell_with_curve(
     curve_pool,
     tokens_for_alice,
     curve_dest,
@@ -94,3 +153,4 @@ def test_buysell_with_curve(
     invoker.invoke([cmove, cswap_curve], [calldata_move, calldata_swap], {"from": alice})
 
     assert curve_dest.balanceOf(invoker) >= amount_out
+"""
