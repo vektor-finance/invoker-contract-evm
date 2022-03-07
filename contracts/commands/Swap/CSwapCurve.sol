1df3aa8d13f24e6f01723a6905b1402badaabd3b
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-03-07 15:02:38 +0000

    feat: implement cswap base

---
+++ contracts/commands/Swap/CSwapCurve.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Vektor SWAP COMMAND (Curve)
+pragma solidity ^0.8.6;
+
+import "../../../interfaces/Commands/Swap/Curve/ICryptoPool.sol";
+import "../../../interfaces/Commands/Swap/Curve/ICSwapCurve.sol";
+import "../../../interfaces/Commands/Swap/Curve/ICurvePool.sol";
+import "./CSwapBase.sol";
+
+contract CSwapCurve is CSwapBase, ICSwapCurve {
+    function _getContractName() internal pure override returns (string memory) {
+        return "CSwapCurve";
+    }
+
+    // https://github.com/curvefi/curve-pool-registry/blob/master/contracts/Swaps.vy#L446
+    function sell(
+        uint256 amountIn,
+        uint256 minAmountOut,
+        address[2] calldata tokens,
+        CurveSwapParams calldata params
+    ) external payable {
+        IERC20 tokenIn = IERC20(tokens[0]);
+        IERC20 tokenOut = IERC20(tokens[1]);
+        tokenIn.safeApprove(params.poolAddress, 0);
+        tokenIn.safeApprove(params.poolAddress, amountIn);
+
+        uint256 balanceBefore = tokenOut.balanceOf(address(this));
+
+        if (params.swapType == 1) {
+            // Stableswap `exchange`
+            ICurvePool(params.poolAddress).exchange(
+                int128(int256(params.tokenI)),
+                int128(int256(params.tokenJ)),
+                amountIn,
+                0
+            );
+        } else if (params.swapType == 2) {
+            // Stableswap `exchange_underlying`
+            ICurvePool(params.poolAddress).exchange_underlying(
+                int128(int256(params.tokenI)),
+                int128(int256(params.tokenJ)),
+                amountIn,
+                0
+            );
+        } else if (params.swapType == 3) {
+            // Cryptoswap `exchange`
+            ICryptoPool(params.poolAddress).exchange(params.tokenI, params.tokenJ, amountIn, 0);
+        } else if (params.swapType == 4) {
+            // Cryptoswap `exchange_underlying`
+            ICryptoPool(params.poolAddress).exchange_underlying(
+                params.tokenI,
+                params.tokenJ,
+                amountIn,
+                0
+            );
+        } else {
+            _revertMsg("Unknown swapType");
+        }
+
+        uint256 balanceAfter = tokenOut.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore + minAmountOut, "CSwap: Slippage in");
+    }
+
+    function buy(
+        uint256 amountOut,
+        uint256 maxAmountIn,
+        address[2] calldata tokens,
+        CurveSwapParams calldata params
+    ) external payable {
+        _revertMsg("buy not supported");
+    }
+
+    // need to consider how to handle native eth (alternatively, enforce wrapped eth)
+}
