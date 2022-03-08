0bce65eec26c839e5ca61aaa4939075131a11122
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-03-08 15:44:33 +0000

    feat: add router to interface

--- interfaces/Commands/Swap/UniswapV3/ICSwapUniswapV3.sol
+++ interfaces/Commands/Swap/UniswapV3/ICSwapUniswapV3.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 interface ICSwapUniswapV3 {
     struct UniswapV3SwapParams {
+        address router;
         bytes path;
         address receiver;
         uint256 deadline;
