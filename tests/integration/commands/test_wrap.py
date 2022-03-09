ccceb850c7ea673d9374fad1b24a694d76060b4a
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-03-09 19:14:40 +0000

    feat: add optimism assets

--- tests/integration/commands/test_wrap.py
+++ tests/integration/commands/test_wrap.py
@@ -13,21 +13,23 @@ def test_wrap_native(alice, invoker, wnative, cwrap, value):
 @given(value=strategy("uint256", max_value="1000 ether"))
 def test_unwrap_native(alice, invoker, wnative, cwrap, value, bob):
     # bob mints wrapped native and sends to invoker
+    start_balance = invoker.balance()
     wnative.deposit({"from": bob, "value": value})
     wnative.transfer(invoker, value, {"from": bob})
     calldata_unwrap = cwrap.unwrapWrappedNative.encode_input(value)
     invoker.invoke([cwrap.address], [calldata_unwrap], {"from": alice})
-    assert invoker.balance() == value
+    assert invoker.balance() == value + start_balance
     assert wnative.balanceOf(invoker) == 0
 
 
 @given(value=strategy("uint256", max_value="1000 ether"))
 def test_unwrap_all_native(alice, invoker, wnative, cwrap, value, bob):
     # bob mints wrapped native and sends to invoker
+    start_balance = invoker.balance()
     wnative.deposit({"from": bob, "value": value})
     wnative.transfer(invoker, value, {"from": bob})
 
     calldata_unwrap_all = cwrap.unwrapAllWrappedNative.encode_input()
     invoker.invoke([cwrap.address], [calldata_unwrap_all], {"from": alice})
-    assert invoker.balance() == value
+    assert invoker.balance() == value + start_balance
     assert wnative.balanceOf(invoker) == 0
