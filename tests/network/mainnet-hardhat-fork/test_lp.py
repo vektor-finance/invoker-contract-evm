4dcfd22e3443297212bdd5ebbd4231ba5e3a0ae7
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-19 10:39:54 +0100

    hit 100% statement coverage

--- tests/network/mainnet-hardhat-fork/test_lp.py
+++ tests/network/mainnet-hardhat-fork/test_lp.py
@@ -1,3 +1,4 @@
+import brownie
 import pytest
 from brownie import ZERO_ADDRESS, Contract, interface
 from eth_account import Account
@@ -84,7 +85,8 @@ def test_permit(invoker, clp, sign_eip2612_permit, chain):
     clp.eip2612Permit(lp_token, owner.address, invoker, 2**256 - 1, deadline, v, hex_r, hex_s)
 
 
-def test_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove):
+@pytest.mark.parametrize("receiver", [ZERO_ADDRESS, "user"])
+def test_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove, receiver):
     # setup assets
     assets = get_chain()["assets"]
     WETH = [asset for asset in assets if asset["symbol"] == "WETH"][0]
@@ -93,6 +95,8 @@ def test_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove):
     usdc = Contract.from_abi(USDC["name"], USDC["address"], interface.ERC20Detailed.abi)
 
     alice = Account.create()
+    if receiver == "user":
+        receiver = alice.address
     # mint for alice
     # approx ~100 usd liquidity
     lp_token = Contract.from_abi(
@@ -118,7 +122,7 @@ def test_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove):
     )
     calldata_move_in = cmove.moveERC20In.encode_input(lp_token, amount_in)
     calldata_withdraw = clp.withdraw.encode_input(
-        lp_token, amount_in, (ZERO_ADDRESS, 0, 0, alice.address, 0)
+        lp_token, amount_in, (ZERO_ADDRESS, 0, 0, receiver, 0)
     )
 
     invoker.invoke(
@@ -127,5 +131,50 @@ def test_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove):
         {"from": alice.address},
     )
 
-    assert weth.balanceOf(alice.address) > 0
-    assert usdc.balanceOf(alice.address) > 0
+    if receiver == ZERO_ADDRESS:
+        output_user = invoker.address
+    else:
+        output_user = alice.address
+    assert weth.balanceOf(output_user) > 0
+    assert usdc.balanceOf(output_user) > 0
+
+
+def test_expired_withdraw(chain, sign_eip2612_permit, invoker, clp, cmove):
+
+    alice = Account.create()
+    # mint for alice
+    # approx ~100 usd liquidity
+    lp_token = Contract.from_abi(
+        "WETH-USDC LP Token",
+        "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc",
+        interface.IUniswapV2Pair.abi,
+    )
+    amount_in = int(1e12)
+    lp_token.transfer(
+        alice.address, amount_in, {"from": "0x03ae53b33feeac1222c3f372f32d37ba95f0f099"}
+    )
+
+    # prepare signature for permit
+    permit_deadline = chain.time() + 100
+    sig = sign_eip2612_permit(
+        lp_token, alice, invoker.address, deadline=permit_deadline, allowance=amount_in
+    )
+    v, hex_r, hex_s = Web3.toInt(sig[-1]), Web3.toHex(sig[:32]), Web3.toHex(sig[32:64])
+
+    # compose invoker transaction
+    calldata_permit = clp.eip2612Permit.encode_input(
+        lp_token, alice.address, invoker, amount_in, permit_deadline, v, hex_r, hex_s
+    )
+    # deadline in past
+    expired_deadline = chain.time() - 100
+    calldata_move_in = cmove.moveERC20In.encode_input(lp_token, amount_in)
+    calldata_withdraw = clp.withdraw.encode_input(
+        lp_token, amount_in, (ZERO_ADDRESS, 0, 0, alice.address, expired_deadline)
+    )
+
+    with brownie.reverts("CLPUniswapV2: EXPIRED"):
+        invoker.invoke(
+            [clp, cmove, clp],
+            [calldata_permit, calldata_move_in, calldata_withdraw],
+            {"from": alice.address},
+        )
