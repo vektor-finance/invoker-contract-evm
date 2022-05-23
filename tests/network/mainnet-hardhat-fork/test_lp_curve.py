446af3593939c9cf624abe3937f5dbb6f0e5a528
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-23 15:36:30 +0200

    working on compound

--- tests/network/mainnet-hardhat-fork/test_lp_curve.py
+++ tests/network/mainnet-hardhat-fork/test_lp_curve.py
@@ -69,48 +69,49 @@ def test_deposit(invoker, cmove, alice, clp_curve):
 
 def test_deposit_zap(invoker, cmove, clp_curve, alice):
     assets = get_chain()["assets"]
+    DAI = [asset for asset in assets if asset["symbol"] == "DAI"][0]
     USDC = [asset for asset in assets if asset["symbol"] == "USDC"][0]
-    USDT = [asset for asset in assets if asset["symbol"] == "USDT"][0]
 
+    dai = Contract.from_abi(DAI["name"], DAI["address"], interface.ERC20Detailed.abi)
     usdc = Contract.from_abi(USDC["name"], USDC["address"], interface.ERC20Detailed.abi)
-    usdt = Contract.from_abi(USDT["name"], USDT["address"], interface.ERC20Detailed.abi)
 
     curve_compound = "0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56"
     curve_pool = Contract.from_abi("Compound pool", curve_compound, interface.ICurvePool.abi)
     curve_zap = "0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06"
 
+    dai_amount = 100e18
     usdc_amount = 100e6
-    usdt_amount = 100e6
 
+    dai.approve(invoker.address, dai_amount, {"from": alice})
     usdc.approve(invoker.address, usdc_amount, {"from": alice})
-    usdt.approve(invoker.address, usdt_amount, {"from": alice})
 
     mint_tokens_for(usdc, alice.address)
-    mint_tokens_for(usdt, alice.address)
+    mint_tokens_for(dai, alice.address)
 
+    calldata_move_dai = cmove.moveERC20In.encode_input(dai.address, dai_amount)
     calldata_move_usdc = cmove.moveERC20In.encode_input(usdc.address, usdc_amount)
-    calldata_move_usdt = cmove.moveERC20In.encode_input(usdt.address, usdt_amount)
 
     # need to specify which function due to function overloading
     expected_amount = curve_pool.calc_token_amount["uint256[2],bool"](
-        [usdc_amount, usdt_amount], True
+        [dai_amount, usdc_amount], True
     )
     min_amount = expected_amount // 1.01
 
     # dai, usdc, usdt
     calldata_deposit = clp_curve.depositZap.encode_input(
-        [usdc_amount, usdt_amount],
-        [usdc, usdt],
+        [dai_amount, usdc_amount],
+        [dai, usdc],
         curve_zap,
         [min_amount],
     )
 
     invoker.invoke(
-        [cmove, cmove, cmove, clp_curve],
-        [calldata_move_usdc, calldata_move_usdt, calldata_deposit],
+        [cmove, cmove, clp_curve],
+        [calldata_move_usdc, calldata_move_dai, calldata_deposit],
         {"from": alice},
     )
 
     token_compound = interface.ERC20Detailed("0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2")
 
     assert token_compound.balanceOf(invoker) >= min_amount
+    assert False
