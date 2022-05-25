13c03ad0ffa036fde0f3b521c9fccf7a9ba66460
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-25 11:29:46 +0200

    refactor basepool test to parametrize

--- tests/network/mainnet-hardhat-fork/test_lp_curve.py
+++ tests/network/mainnet-hardhat-fork/test_lp_curve.py
@@ -13,58 +13,99 @@ def clp_curve(invoker, deployer, CLPCurve):
     yield contract
 
 
-def test_deposit(invoker, cmove, alice, clp_curve):
-    assets = get_chain()["assets"]
-    USDC = [asset for asset in assets if asset["symbol"] == "USDC"][0]
-    DAI = [asset for asset in assets if asset["symbol"] == "DAI"][0]
-    USDT = [asset for asset in assets if asset["symbol"] == "USDT"][0]
+params_3pool = (
+    ["DAI", "USDC", "USDT"],
+    "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7",
+    "0x6c3f90f043a72fa612cbac8115ee7e52bde6e490",
+    "0xd632f22692fac7611d2aa1c0d552930d43caed3b",
+)
 
-    usdc = Contract.from_abi(USDC["name"], USDC["address"], interface.ERC20Detailed.abi)
-    dai = Contract.from_abi(DAI["name"], DAI["address"], interface.ERC20Detailed.abi)
-    usdt = Contract.from_abi(USDT["name"], USDT["address"], interface.ERC20Detailed.abi)
 
-    curve_3pool = "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7"
-    curve_pool = Contract.from_abi("Curve 3pool", curve_3pool, interface.ICurvePool.abi)
+@pytest.mark.parametrize("tokens,curve_pool,lp_token,lp_benefactor", [params_3pool])
+class TestBasePool:
+    def test_deposit(
+        self, tokens, curve_pool, lp_token, alice, invoker, cmove, clp_curve, lp_benefactor
+    ):
+        assets = get_chain()["assets"]
+        token_contracts = []
+        token_amounts = []
+        calldatas = []
+        for token in tokens:
+            _token = [asset for asset in assets if asset["symbol"] == token][0]
+            _contract = interface.ERC20Detailed(_token["address"])
+            _amount = 100 * 10 ** _token["decimals"]
+            token_contracts.append(_contract)
+            token_amounts.append(_amount)
+            _contract.approve(invoker.address, _amount, {"from": alice})
+            mint_tokens_for(_contract, alice.address)
+            calldatas.append(cmove.moveERC20In.encode_input(_contract, _amount))
 
-    usdc_amount = 100e6
-    dai_amount = 100e18
-    usdt_amount = 100e6
+        curve_pool = interface.ICurvePool(curve_pool)
 
-    usdc.approve(invoker.address, usdc_amount, {"from": alice})
-    dai.approve(invoker.address, dai_amount, {"from": alice})
-    usdt.approve(invoker.address, usdt_amount, {"from": alice})
+        # need to specify which function due to function overloading
+        expected_amount = curve_pool.calc_token_amount[f"uint256[{len(tokens)}],bool"](
+            token_amounts, True
+        )
+        min_amount = expected_amount // 1.01
 
-    mint_tokens_for(usdc, alice.address)
-    mint_tokens_for(dai, alice.address)
-    mint_tokens_for(usdt, alice.address)
+        # dai, usdc, usdt
+        calldata_deposit = clp_curve.deposit.encode_input(
+            token_amounts,
+            token_contracts,
+            curve_pool,
+            [min_amount],
+        )
 
-    calldata_move_usdc = cmove.moveERC20In.encode_input(usdc.address, usdc_amount)
-    calldata_move_dai = cmove.moveERC20In.encode_input(dai.address, dai_amount)
-    calldata_move_usdt = cmove.moveERC20In.encode_input(usdt.address, usdt_amount)
+        invoker.invoke(
+            [*[cmove] * len(tokens), clp_curve],
+            [*calldatas, calldata_deposit],
+            {"from": alice},
+        )
 
-    # need to specify which function due to function overloading
-    expected_amount = curve_pool.calc_token_amount["uint256[3],bool"](
-        [dai_amount, usdc_amount, usdt_amount], True
-    )
-    min_amount = expected_amount // 1.01
+        lp_token = interface.ERC20Detailed(lp_token)
 
-    # dai, usdc, usdt
-    calldata_deposit = clp_curve.deposit.encode_input(
-        [dai_amount, usdc_amount, usdt_amount],
-        [dai, usdc, usdt],
-        curve_pool,
-        [min_amount],
-    )
+        assert lp_token.balanceOf(invoker) >= min_amount
 
-    invoker.invoke(
-        [cmove, cmove, cmove, clp_curve],
-        [calldata_move_usdc, calldata_move_dai, calldata_move_usdt, calldata_deposit],
-        {"from": alice},
-    )
+    def test_withdraw(
+        self, invoker, cmove, alice, clp_curve, tokens, curve_pool, lp_token, lp_benefactor
+    ):
+        assets = get_chain()["assets"]
 
-    token_3pool = interface.ERC20Detailed("0x6c3f90f043a72fa612cbac8115ee7e52bde6e490")
+        curve_pool = interface.CurvePool(curve_pool)
+        lp_token = interface.ERC20Detailed(lp_token)
 
-    assert token_3pool.balanceOf(invoker) >= min_amount
+        lp_amount = 100e18
+        lp_ratio = lp_amount / lp_token.totalSupply()
+
+        token_contracts = []
+        pool_balances = []
+        min_tokens_received = []
+        for key, token in enumerate(tokens):
+            _token = [asset for asset in assets if asset["symbol"] == token][0]
+            _contract = interface.ERC20Detailed(_token["address"])
+            token_contracts.append(_contract)
+            _balance = curve_pool.balances(key)
+            pool_balances.append(_balance)
+            min_tokens_received.append(lp_ratio * _balance * 0.99)
+
+        lp_token.transfer(alice, lp_amount, {"from": lp_benefactor})
+        lp_token.approve(invoker, lp_amount, {"from": alice})
+
+        calldata_move = cmove.moveERC20In.encode_input(lp_token, lp_amount)
+        calldata_withdraw = clp_curve.withdraw.encode_input(
+            curve_pool,
+            lp_amount,
+            min_tokens_received,
+        )
+
+        invoker.invoke([cmove, clp_curve], [calldata_move, calldata_withdraw], {"from": alice})
+
+        for token, min_received in zip(token_contracts, min_tokens_received):
+            assert token.balanceOf(invoker) >= min_received
+
+
+class TestUnderlyingPool:
+    pass
 
 
 def test_deposit_zap(invoker, cmove, clp_curve, alice):
@@ -122,48 +163,6 @@ def test_deposit_zap(invoker, cmove, clp_curve, alice):
     token_compound = interface.ERC20Detailed("0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2")
 
     assert token_compound.balanceOf(invoker) >= min_amount
-
-
-def test_withdraw(invoker, cmove, alice, clp_curve):
-    whale_3pool = "0xd632f22692fac7611d2aa1c0d552930d43caed3b"
-    assets = get_chain()["assets"]
-    DAI = [asset for asset in assets if asset["symbol"] == "DAI"][0]
-    USDC = [asset for asset in assets if asset["symbol"] == "USDC"][0]
-    USDT = [asset for asset in assets if asset["symbol"] == "USDT"][0]
-
-    dai = Contract.from_abi(DAI["name"], DAI["address"], interface.ERC20Detailed.abi)
-    usdc = Contract.from_abi(USDC["name"], USDC["address"], interface.ERC20Detailed.abi)
-    usdt = Contract.from_abi(USDT["name"], USDT["address"], interface.ERC20Detailed.abi)
-
-    curve_3pool = "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7"
-    curve_pool = Contract.from_abi("Curve 3pool", curve_3pool, interface.CurvePool.abi)
-    token_3pool = interface.ERC20Detailed("0x6c3f90f043a72fa612cbac8115ee7e52bde6e490")
-
-    dai_total_balance = curve_pool.balances(0)
-    usdc_total_balance = curve_pool.balances(1)
-    usdt_total_balance = curve_pool.balances(2)
-
-    lp_amount = 100e18
-    lp_ratio = lp_amount / token_3pool.totalSupply()
-
-    min_dai_received = int(lp_ratio * dai_total_balance * 0.99)
-    min_usdc_received = int(lp_ratio * usdc_total_balance * 0.99)
-    min_usdt_received = int(lp_ratio * usdt_total_balance * 0.99)
-
-    token_3pool.transfer(alice, lp_amount, {"from": whale_3pool})
-
-    token_3pool.approve(invoker, lp_amount, {"from": alice})
-    calldata_move = cmove.moveERC20In.encode_input(token_3pool, lp_amount)
-    calldata_withdraw = clp_curve.withdraw.encode_input(
-        curve_pool,
-        lp_amount,
-        [min_dai_received, min_usdc_received, min_usdt_received],
-    )
-
-    invoker.invoke([cmove, clp_curve], [calldata_move, calldata_withdraw], {"from": alice})
-    assert dai.balanceOf(invoker) >= min_dai_received
-    assert usdc.balanceOf(invoker) >= min_usdc_received
-    assert usdt.balanceOf(invoker) >= min_usdt_received
 
 
 def test_withdraw_zap(invoker, cmove, alice, clp_curve):
