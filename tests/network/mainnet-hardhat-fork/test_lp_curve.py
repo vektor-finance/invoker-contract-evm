71f76c9085fb873841145308e55e7ac4190ca958
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-25 13:33:34 +0200

    parametrised withdraw zap

--- tests/network/mainnet-hardhat-fork/test_lp_curve.py
+++ tests/network/mainnet-hardhat-fork/test_lp_curve.py
@@ -175,11 +175,68 @@ class UnderlyingPool:
         lp_token = interface.ERC20Detailed(lp_token)
         assert lp_token.balanceOf(invoker) >= min_amount
 
+    def test_withdraw_zap(
+        self,
+        tokens,
+        curve_pool,
+        curve_zap,
+        invoker,
+        alice,
+        cmove,
+        clp_curve,
+        lp_token,
+        lp_benefactor,
+    ):
+        assets = get_chain()["assets"]
+
+        curve_pool = Contract.from_abi(
+            "Curve Pool",
+            curve_pool,
+            [
+                {
+                    "constant": True,
+                    "gas": 2250,
+                    "inputs": [{"name": "arg0", "type": "int128"}],
+                    "name": "balances",
+                    "outputs": [{"name": "out", "type": "uint256"}],
+                    "payable": False,
+                    "type": "function",
+                }
+            ],
+        )
+        lp_token = interface.ERC20Detailed(lp_token)
+
+        lp_amount = 100e18
+
+        token_contracts = []
+        for token in tokens:
+            _token = [asset for asset in assets if asset["symbol"] == token][0]
+            _address = _token["address"] or "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+            _contract = interface.ERC20Detailed(_address)
+            token_contracts.append(_contract)
+
+        lp_token.transfer(alice, lp_amount, {"from": lp_benefactor})
+        lp_token.approve(invoker, lp_amount, {"from": alice})
+
+        min_tokens_received = self.calc_withdraw(curve_pool, lp_amount, lp_token)
+
+        calldata_move = cmove.moveERC20In.encode_input(lp_token, lp_amount)
+        calldata_withdraw = clp_curve.withdrawZap.encode_input(
+            lp_token,
+            curve_zap,
+            lp_amount,
+            min_tokens_received,
+        )
+
+        invoker.invoke([cmove, clp_curve], [calldata_move, calldata_withdraw], {"from": alice})
+
+        for token, min_received in zip(token_contracts, min_tokens_received):
+            assert token.balanceOf(invoker) >= min_received
+
 
 @pytest.mark.parametrize("tokens,curve_pool,curve_zap,lp_token,lp_benefactor", [params_compound])
 class TestCompoundPool(UnderlyingPool):
     def calc_deposit(self, curve_pool, token_contracts, token_amounts):
-        # todo: do this
         cdai = interface.CToken("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643")
         cusdc = interface.CToken("0x39AA39c021dfbaE8faC545936693aC917d5E7563")
         cdai_amount = int(token_amounts[0] * 1e18 / cdai.exchangeRateStored())
@@ -189,59 +246,18 @@ class TestCompoundPool(UnderlyingPool):
         )
         return expected_amount
 
+    def calc_withdraw(self, curve_pool, lp_amount, lp_token):
+        cdai = interface.CToken("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643")
+        cusdc = interface.CToken("0x39AA39c021dfbaE8faC545936693aC917d5E7563")
 
-def test_withdraw_zap(invoker, cmove, alice, clp_curve):
-    assets = get_chain()["assets"]
-    DAI = [asset for asset in assets if asset["symbol"] == "DAI"][0]
-    USDC = [asset for asset in assets if asset["symbol"] == "USDC"][0]
+        # To calculate your balance in the underlying asset,
+        # multiply your cToken balance by exchangeRateStored, and divide by 1e18.
+        dai_total_balance = curve_pool.balances(0) * cdai.exchangeRateStored() / 1e18
+        usdc_total_balance = curve_pool.balances(1) * cusdc.exchangeRateStored() / 1e18
 
-    dai = Contract.from_abi(DAI["name"], DAI["address"], interface.ERC20Detailed.abi)
-    usdc = Contract.from_abi(USDC["name"], USDC["address"], interface.ERC20Detailed.abi)
+        lp_ratio = lp_amount / lp_token.totalSupply()
 
-    curve_compound = "0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56"
-    curve_pool = Contract.from_abi(
-        "Compound pool",
-        curve_compound,
-        [
-            {
-                "constant": True,
-                "gas": 2250,
-                "inputs": [{"name": "arg0", "type": "int128"}],
-                "name": "balances",
-                "outputs": [{"name": "out", "type": "uint256"}],
-                "payable": False,
-                "type": "function",
-            }
-        ],
-    )
-    curve_zap = "0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06"
-    token_compound = interface.ERC20Detailed("0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2")
-    whale_compound = "0x7ca5b0a2910b33e9759dc7ddb0413949071d7575"
+        min_dai_received = int(lp_ratio * dai_total_balance * 0.99)
+        min_usdc_received = int(lp_ratio * usdc_total_balance * 0.99)
 
-    cdai = interface.CToken("0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643")
-    cusdc = interface.CToken("0x39AA39c021dfbaE8faC545936693aC917d5E7563")
-
-    # To calculate your balance in the underlying asset,
-    # multiply your cToken balance by exchangeRateStored, and divide by 1e18.
-    dai_total_balance = curve_pool.balances(0) * cdai.exchangeRateStored() / 1e18
-    usdc_total_balance = curve_pool.balances(1) * cusdc.exchangeRateStored() / 1e18
-
-    lp_amount = 100e18
-    lp_ratio = lp_amount / token_compound.totalSupply()
-
-    min_dai_received = int(lp_ratio * dai_total_balance * 0.99)
-    min_usdc_received = int(lp_ratio * usdc_total_balance * 0.99)
-
-    # mint LP tokens for alice
-    token_compound.transfer(alice, lp_amount, {"from": whale_compound})
-
-    token_compound.approve(invoker, lp_amount, {"from": alice})
-
-    calldata_move_in = cmove.moveERC20In.encode_input(token_compound, lp_amount)
-    calldata_withdraw_zap = clp_curve.withdrawZap.encode_input(
-        token_compound, curve_zap, lp_amount, [min_dai_received, min_usdc_received]
-    )
-
-    invoker.invoke([cmove, clp_curve], [calldata_move_in, calldata_withdraw_zap], {"from": alice})
-    assert dai.balanceOf(invoker) >= min_dai_received
-    assert usdc.balanceOf(invoker) >= min_usdc_received
+        return [min_dai_received, min_usdc_received]
