46197fdeda279408e957a1a79f650bac9e0d461c
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-27 12:29:56 +0100

    update assertions for test

--- tests/network/mainnet-hardhat-fork/test_lp_uniswapv3.py
+++ tests/network/mainnet-hardhat-fork/test_lp_uniswapv3.py
@@ -13,8 +13,13 @@ def clp_uniswapv3(invoker, deployer, CLPUniswapV3):
 
 
 def test_mint(invoker, clp_uniswapv3, alice, cmove, chain):
+    nftm = interface.NonfungiblePositionManager("0xc36442b4a4522e871399cd717abdd847ab11fe88")
+
+    tick_lower = 0
+    tick_upper = 6000
+
     # WETH-USDC 0.3%
-    uniswap_pool = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+    uniswap_pool = interface.IUniswapV3Pool("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8")
 
     usdc = interface.ERC20Detailed("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
     weth = interface.ERC20Detailed("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
@@ -33,16 +38,27 @@ def test_mint(invoker, clp_uniswapv3, alice, cmove, chain):
 
     calldata_deposit = clp_uniswapv3.depositNew.encode_input(
         uniswap_pool,
-        0,
-        6000,
+        tick_lower,
+        tick_upper,
         usdc_amount,
         weth_amount,
-        ("0xc36442b4a4522e871399cd717abdd847ab11fe88", 0, 0, alice, chain.time() + 100),
+        (nftm, 0, 0, alice, chain.time() + 100),
     )
-    # Contract.from_explorer("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8")
 
-    invoker.invoke(
+    tx = invoker.invoke(
         [cmove, cmove, clp_uniswapv3],
         [calldata_move_usdc, calldata_move_weth, calldata_deposit],
         {"from": alice},
     )
+
+    assert "IncreaseLiquidity" in tx.events
+    token_id = tx.events["IncreaseLiquidity"]["tokenId"]
+
+    position = nftm.positions(token_id).dict()
+    assert position["token0"] == usdc
+    assert position["token1"] == weth
+    assert position["fee"] == 3000
+    assert position["tickLower"] == tick_lower
+    assert position["tickUpper"] == tick_upper
+
+    assert nftm.ownerOf(token_id) == alice
