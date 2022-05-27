1e66381158dcc307c6b58ea5926ea872519f80b2
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-05-27 13:27:54 +0100

    withdraw liquidity func

--- contracts/commands/LP/CLPUniswapV3.sol
+++ contracts/commands/LP/CLPUniswapV3.sol
@@ -22,6 +22,7 @@ interface INonfungiblePositionManager {
         uint256 amount1Min;
         uint256 deadline;
     }
+
     struct MintParams {
         address token0;
         address token1;
@@ -34,6 +35,21 @@ interface INonfungiblePositionManager {
         uint256 amount1Min;
         address recipient;
         uint256 deadline;
+    }
+
+    struct DecreaseLiquidityParams {
+        uint256 tokenId;
+        uint128 liquidity;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        uint256 deadline;
+    }
+
+    struct CollectParams {
+        uint256 tokenId;
+        address recipient;
+        uint128 amount0Max;
+        uint128 amount1Max;
     }
 
     function mint(MintParams calldata params)
@@ -72,10 +88,29 @@ interface INonfungiblePositionManager {
             uint256 amount0,
             uint256 amount1
         );
+
+    function decreaseLiquidity(DecreaseLiquidityParams calldata params)
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1);
+
+    function collect(CollectParams calldata params)
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1);
+
+    function burn(uint256 tokenId) external payable;
 }
 
 interface ICLPUniswapV3 {
     struct UniswapV3LPDepositParams {
+        address router;
+        uint256 amountAMin;
+        uint256 amountBMin;
+        address receiver;
+        uint256 deadline;
+    }
+    struct UniswapV3LPWithdrawParams {
         address router;
         uint256 amountAMin;
         uint256 amountBMin;
@@ -89,6 +124,9 @@ contract CLPUniswapV3 is CLPBase, ICLPUniswapV3 {
         return "CLPUniswapV3";
     }
 
+    /**
+        @dev PositionManager ensures NFT not minted to zero address
+     **/
     function deposit(
         uint256 tokenId,
         uint256 amountA,
@@ -125,6 +163,9 @@ contract CLPUniswapV3 is CLPBase, ICLPUniswapV3 {
         );
     }
 
+    /**
+        @dev PositionManager ensures NFT not minted to zero address
+     **/
     function depositNew(
         IUniswapV3Pool pool,
         int24 tickLower,
@@ -153,4 +194,33 @@ contract CLPUniswapV3 is CLPBase, ICLPUniswapV3 {
             })
         );
     }
+
+    function withdraw(
+        uint256 tokenId,
+        uint128 liquidity,
+        UniswapV3LPWithdrawParams calldata params
+    ) external payable {
+        INonfungiblePositionManager(params.router).decreaseLiquidity(
+            INonfungiblePositionManager.DecreaseLiquidityParams({
+                tokenId: tokenId,
+                liquidity: liquidity,
+                amount0Min: params.amountAMin,
+                amount1Min: params.amountBMin,
+                deadline: params.deadline
+            })
+        );
+        INonfungiblePositionManager(params.router).collect(
+            INonfungiblePositionManager.CollectParams({
+                tokenId: tokenId,
+                recipient: params.receiver,
+                amount0Max: type(uint128).max,
+                amount1Max: type(uint128).max
+            })
+        );
+    }
+
+    function withdrawAll(uint256 tokenId, UniswapV3LPWithdrawParams calldata params)
+        external
+        payable
+    {}
 }
