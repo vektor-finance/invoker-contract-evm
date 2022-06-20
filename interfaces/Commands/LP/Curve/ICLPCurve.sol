999958d14b7760ee851203183a6b3b6bda542686
abdullathedruid <abdulla@vektor.finance>
2022-06-20 15:20:45 +0100

    Update interfaces/Commands/LP/Curve/ICLPCurve.sol
    Co-authored-by: Akram Hussein <akram@vektor.finance>

--- interfaces/Commands/LP/Curve/ICLPCurve.sol
+++ interfaces/Commands/LP/Curve/ICLPCurve.sol
@@ -5,21 +5,21 @@ pragma solidity ^0.8.6;
 interface ICLPCurve {
     /**
         @notice Used to specify the necessary parameters for a Curve interaction
-        @dev Users may either deposit/withdraw 'base' assets or 'underlying' assets into/from a curve pool.
-            Some curve pools allow directly depositing 'underlying' assets, whilst other curve pools necessitate
+        @dev Users may either deposit/withdraw 'base' assets or 'underlying' assets into/from a Curve pool.
+            Some Curve pools allow directly depositing 'underlying' assets, whilst other Curve pools necessitate
             the usage of a 'helper' contract. 
-            In order to accomodate different behaviour between different curve pools, the caller of this contract
+            In order to accomodate different behaviour between different Curve pools, the caller of this contract
             must explicitly state the desired behaviour:
 
-            If depositing/withdrawing 'base' assets, this parameter MUST be BASE and the `curveDepositAddress`/`curveWithdrawAddress` parameter MUST be the address of the curve contract
+            If depositing/withdrawing 'base' assets, this parameter MUST be BASE and the `curveDepositAddress`/`curveWithdrawAddress` parameter MUST be the address of the Curve contract.
 
-            If depositing/withdrawing 'underlying assets', check whether the curve contract supports underlying assets:
-                If underlying assets are supported, this parameter MUST be UNDERLYING and the `curveDepositAddress`/`curveWithdrawAddress` parameter MUST be the address of the curve contract
+            If depositing/withdrawing 'underlying assets', check whether the Curve contract supports underlying assets:
+                If underlying assets are supported, this parameter MUST be UNDERLYING and the `curveDepositAddress`/`curveWithdrawAddress` parameter MUST be the address of the Curve contract.
                 If underlying assets are not supported, this parameter MUST be CONTRACT and the `curveDepositAddress`/`curveWithdrawAddress` parameter MUST be the address of the helper 'Deposit.vy' contract.
 
-        @param BASE The user is interacting directly with the curve contract and depositing base assets.
-        @param UNDERLYING The user is interacting directly with the curve contract and depositing underlying assets.
-        @param HELPER The user is interacting with a curve `Deposit.vy` contract, and depositing underlying assets.
+        @param BASE The user is interacting directly with the Curve contract and depositing base assets.
+        @param UNDERLYING The user is interacting directly with the Curve contract and depositing underlying assets.
+        @param HELPER The user is interacting with a Curve `Deposit.vy` contract, and depositing underlying assets.
     */
     enum CurveLPType {
         BASE,
