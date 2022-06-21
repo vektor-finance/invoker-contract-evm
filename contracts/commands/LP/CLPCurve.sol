b00c64a1de983cdc52bb9cf2672df5b2aeb79dff
abdullathedruid <abdulla@vektor.finance>
2022-06-21 09:30:11 +0100

    feat(LP): add Curve support (#112)
    * structure of contract
    
    * static length for curve pool
    
    * test case for 3pool
    
    * deposit zap
    
    * test zap
    
    * working on compound
    
    * withdraw contract
    
    * test for withdraw
    
    * fix withdraw test balances
    
    * withdraw zap
    
    * withdraw zap
    
    * update wiithdraw zap test to calculate correct underlying balance
    
    * fix zap deposit min received
    
    * refactor basepool test to parametrize
    
    * test parametrised basepool
    
    * parametrise zap deposit
    
    * parametrised withdraw zap
    
    * remove steth from data
    
    * failing case with aave pool
    
    * refactor depositZap to handle useUnderlying flag
    
    * fix withdraw zap
    
    * parametrise by test case
    
    * update test ids
    
    * tdd for 4pool
    
    * test 4pool
    
    * update error message
    
    * update slippage check
    
    * refactor tests
    
    * disable metapool test
    
    * rename zap
    
    * pin hardhat v
    
    * consistent naming
    
    * internal contract change
    
    * specify token address before token amount
    
    * add slippage constants to test
    
    * add balance helper function
    
    * Refactor curve underlying (#120)
    
    * vary flags for deposit
    
    * fix withdraw with flag
    
    * add context manager for failing test
    
    * test meta pool with gusd
    
    * remove susd
    
    * remove HELPER_CONTRACT_UNDERLYING
    
    * rename curve lptype enum
    
    * move DisableTrace to helper file
    
    * consistent variable name
    
    * update compatability
    
    * fix typo
    
    * move depositAddress to params, and update natspec
    
    * rename enum to base,underlying,helper
    
    * Update interfaces/Commands/LP/Curve/ICLPCurve.sol
    
    Co-authored-by: Akram Hussein <akram@vektor.finance>
    
    * ensure lptype is caught in else, and test
    
    Co-authored-by: Akram Hussein <akram@vektor.finance>

---
+++ contracts/commands/LP/CLPCurve.sol
@@ -0,0 +1,155 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.6;
+
+import "../../../interfaces/Commands/Swap/Curve/ICryptoPool.sol";
+import "../../../interfaces/Commands/Swap/Curve/ICurvePool.sol";
+import "../../../interfaces/Commands/LP/Curve/ICLPCurve.sol";
+import "../../../interfaces/Commands/LP/Curve/ICurveDepositZap.sol";
+import "./CLPBase.sol";
+
+contract CLPCurve is CLPBase, ICLPCurve {
+    function _getContractName() internal pure override returns (string memory) {
+        return "CLPCurve";
+    }
+
+    function deposit(
+        IERC20[] calldata tokens,
+        uint256[] calldata amounts,
+        CurveLPDepositParams calldata params
+    ) external payable {
+        _requireMsg(amounts.length == tokens.length, "amounts+tokens length not equal");
+        // for loop `i` cannot overflow, so we use unchecked block to save gas
+        unchecked {
+            for (uint256 i; i < tokens.length; ++i) {
+                if (amounts[i] > 0) {
+                    _approveToken(tokens[i], params.curveDepositAddress, amounts[i]);
+                }
+            }
+        }
+        if (amounts.length == 2) {
+            uint256[2] memory _tokenAmounts = [amounts[0], amounts[1]];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else if (amounts.length == 3) {
+            uint256[3] memory _tokenAmounts = [amounts[0], amounts[1], amounts[2]];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else if (amounts.length == 4) {
+            uint256[4] memory _tokenAmounts = [amounts[0], amounts[1], amounts[2], amounts[3]];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveDepositAddress).add_liquidity(
+                    _tokenAmounts,
+                    params.minReceivedLiquidity,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else {
+            _revertMsg("unsupported length");
+        }
+    }
+
+    function withdraw(
+        IERC20 LPToken,
+        uint256 liquidity,
+        CurveLPWithdrawParams calldata params
+    ) external payable {
+        if (params.lpType == CurveLPType.HELPER) {
+            _approveToken(LPToken, params.curveWithdrawAddress, liquidity);
+        }
+        if (params.minimumReceived.length == 2) {
+            uint256[2] memory _tokenAmounts = [
+                params.minimumReceived[0],
+                params.minimumReceived[1]
+            ];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else if (params.minimumReceived.length == 3) {
+            uint256[3] memory _tokenAmounts = [
+                params.minimumReceived[0],
+                params.minimumReceived[1],
+                params.minimumReceived[2]
+            ];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else if (params.minimumReceived.length == 4) {
+            uint256[4] memory _tokenAmounts = [
+                params.minimumReceived[0],
+                params.minimumReceived[1],
+                params.minimumReceived[2],
+                params.minimumReceived[3]
+            ];
+            if (params.lpType == CurveLPType.BASE || params.lpType == CurveLPType.HELPER) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts
+                );
+            } else if (params.lpType == CurveLPType.UNDERLYING) {
+                ICurveDepositZap(params.curveWithdrawAddress).remove_liquidity(
+                    liquidity,
+                    _tokenAmounts,
+                    true
+                );
+            } else {
+                _revertMsg("invalid lpType");
+            }
+        } else {
+            _revertMsg("unsupported length");
+        }
+    }
+}
