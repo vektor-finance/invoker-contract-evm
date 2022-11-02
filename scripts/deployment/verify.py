8e5e28a75434749dfdb75084711c8ca919cd221d
Abdulla Al-Kamil <abdulla@vektor.finance>
2022-11-02 16:27:01 +0000

    feat: update verify script

--- scripts/deployment/verify.py
+++ scripts/deployment/verify.py
@@ -1,8 +1,17 @@
 import json
+import time
 from pathlib import Path
 
 import brownie
+from brownie import CLendAave, CLendCompoundV3
 from brownie.network.contract import ContractContainer
+
+from scripts.deployment import (
+    ALL_CONTRACTS,
+    REGISTRY_DEPLOYER,
+    TRUSTED_DEPLOYER,
+    DeployRegistryContainer,
+)
 
 
 def is_modified():
@@ -20,15 +29,35 @@ def is_modified():
 
 
 def verify_contract(contract: ContractContainer, address: str):
+    print(f"Attempting to verify {contract._name} at {address}")
     to_verify = contract.at(address)
     assert contract.get_verification_info()["license_identifier"].lower() == "unlicensed"
-    contract.publish_source(to_verify)
+    try:
+        contract.publish_source(to_verify)
+        time.sleep(5)  # don't throttle API
+    except ValueError as e:
+        msg = e.args[0]
+        if "Contract source code already verified" not in msg:
+            raise e from None
+        print(f"Already verified {contract._name}")
 
 
 def main():
     if is_modified():
-        # need to actually add arguments here
-        verify_contract()
+        registry = DeployRegistryContainer(
+            REGISTRY_DEPLOYER, TRUSTED_DEPLOYER, ensure_deployed=True
+        )
+        for contract in ALL_CONTRACTS:
+            if contract not in [
+                CLendAave,
+                CLendCompoundV3,
+            ]:  # can't verify vyper contracts via etherscan API
+                verify_contract(contract, registry.get_deployed_contract(contract))
+            else:
+                print(
+                    f"Need to manually verify {contract._name} at "
+                    f"{registry.get_deployed_contract(contract)}"
+                )
     else:
         print("Not using patched version of brownie")
         print(
