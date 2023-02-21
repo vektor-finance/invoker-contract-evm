bca28df7dfddfbfdc3aab9551086fdebd0aace28
Abdulla Al-Kamil <abdulla@vektor.finance>
2023-02-21 15:02:59 +0000

    add interface

--- interfaces/Commands/Lend/IAaveToken.sol
+++ interfaces/Commands/Lend/IAaveToken.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.9;
 
 interface IAaveToken {
     function UNDERLYING_ASSET_ADDRESS() external returns (address tokenAddress);
+
+    function balanceOf(address account) external view returns (uint256);
 }
